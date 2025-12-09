b206feca63b074ed748b44f15da1db10a9e91492
Holysmasher <shawnsummers@live.com>
2025-12-08 17:31:52 -0700

    Binding Player Class info to XAML

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -7,15 +7,20 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    internal class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Hero";
             CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.CharacterClass = "Warrior";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
         }
     }
 }
